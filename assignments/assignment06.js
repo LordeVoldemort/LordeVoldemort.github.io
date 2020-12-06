493c9d67d46b10463e6ccdf5a84e8bf8e1bf4a41
LordeVoldemort <70917366+LordeVoldemort@users.noreply.github.com>
2020-12-06 03:09:49 -0500

    Update assignment06.js

--- assignments/assignment06.js
+++ assignments/assignment06.js
@@ -1,30 +1,11 @@
 // --- global variables ---
-var loans = [{
-        loan_year: 2020,
-        loan_amount: 10000.00,
-        loan_int_rate: 0.0453
-    },
-    {
-        loan_year: 2021,
-        loan_amount: 10000.00,
-        loan_int_rate: 0.0453
-    },
-    {
-        loan_year: 2022,
-        loan_amount: 10000.00,
-        loan_int_rate: 0.0453
-    },
-    {
-        loan_year: 2023,
-        loan_amount: 10000.00,
-        loan_int_rate: 0.0453
-    },
-    {
-        loan_year: 2024,
-        loan_amount: 10000.00,
-        loan_int_rate: 0.0453
-    }
-];
+var loans = [
+  { loan_year: 2020, loan_amount: 10000.00, loan_int_rate: 0.0453 },
+  { loan_year: 2021, loan_amount: 10000.00, loan_int_rate: 0.0453 },
+  { loan_year: 2022, loan_amount: 10000.00, loan_int_rate: 0.0453 },
+  { loan_year: 2023, loan_amount: 10000.00, loan_int_rate: 0.0453 },
+  { loan_year: 2024, loan_amount: 10000.00, loan_int_rate: 0.0453 }
+]; 
 
 var loanAmount = 57219.62;
 
